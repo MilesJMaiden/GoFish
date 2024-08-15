c3150c7e96e4b2d95ab6f6739b0a185a2b36c6ff
Runtomaitorun <yeahmy1022@gmail.com>
2024-08-15 17:49:40 +0100

    Meta avatar issued ver

---
+++ GoFish/Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    //[Networked]
+    //public int Score { get; set; }
+
+
+    //void 
+}
